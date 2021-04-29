a389748a50f810e1d0f8ea8414baf6a701c724cb
Ashish-884061 <zashish9686@gmail.com>
2021-04-29 14:12:43 +0530

    first

---
+++ Printtable.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Printtable {
+
+	public static void main(String[] args) {
+		Hello table= new Hello();
+		//table.Print();
+		//table.Print(6);
+		table.Print(6,2,15);
+
+	}
+
+}
